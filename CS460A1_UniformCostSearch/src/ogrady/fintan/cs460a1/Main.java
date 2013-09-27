23e78d212ee43eca8c8bc0173cbc8a6b2aa7f633
Fintan O'Grady <jtkirker@gmail.com>
2013-09-27 15:32:06 -0700

    Waterfalled! All done, not tested.

--- CS460A1_UniformCostSearch/src/ogrady/fintan/cs460a1/Main.java
+++ CS460A1_UniformCostSearch/src/ogrady/fintan/cs460a1/Main.java
@@ -1,5 +1,18 @@
 package ogrady.fintan.cs460a1;
 
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -11,6 +24,63 @@ public class Main {
 	
 	public static void main(String[] args) {
 
+		List<String> inputLines;
+		
+		Path inPath = FileSystems.getDefault().getPath("input.txt");
+		Path outPath = FileSystems.getDefault().getPath("output.txt");
+		
+		try {
+			inputLines = Files.readAllLines(
+					inPath,
+					Charset.defaultCharset());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		int boardSize 	= Integer.parseInt(inputLines.get(0));
+		String line2	= inputLines.get(1);
+		int ax			= Integer.parseInt(line2.split(" ")[0]);
+		int ay			= Integer.parseInt(line2.split(" ")[1]);
+		String line3	= inputLines.get(2);
+		int bx			= Integer.parseInt(line3.split(" ")[0]);
+		int by			= Integer.parseInt(line3.split(" ")[1]);
+		
+		Main m = new Main();
+		Node end = m.findPath(boardSize, ax, ay, bx, by);
+
+		if (end == null) {
+			System.out.println("Could not find a path");
+			return;
+		}
+		
+		//With the end node, we can follow the parent nodes to get the
+		//	path that got us here.
+		//It's in reverse order, though, so we have to reverse it before
+		// 	writing.
+		Deque<Node> out = new ArrayDeque<Node>();
+		
+		out.add(end);
+		Node cur = end;
+		while (cur.parent != null) {
+			//addFirst puts it in correct order, with root first0
+			out.addFirst(cur.parent);
+			cur = cur.parent;
+		}
+		
+		//Now with a reverse-sorted queue, we iterate backwards and 
+		// write to the out file 
+		for (Node n : out) {
+			try {
+				Files.write(outPath, 
+						(n.x+" "+n.y+System.getProperty("line.separator")).getBytes(),
+						//Charset.defaultCharset(),
+						StandardOpenOption.APPEND);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
@@ -41,20 +111,43 @@ public class Main {
 		//Though we still have to log(n) remove the "cheapest" node, meaning
 		//	2log(n). Hmm. There's no built-in OrderedQueue, though, and I don't
 		//	want to have to roll my own just to have a constant-time pop().
-		TreeSet<Node> availableDecisions = new TreeSet<Node>();
+		//We have to set the comparator upon creation, to set how things will get
+		//	ordered upon insertion
+		TreeSet<Node> availableDecisions = new TreeSet<Node>(new Comparator<Node>() {
+			//Upon insertion, nodes with a lower cost should go first.
+			//Upon same cost, we could do something here to make it more deterministic,
+			//	but we won't bother.
+			@Override
+			public int compare(Node arg0, Node arg1) {
+				return arg0.cost - arg1.cost;
+			}
+			
+		});
+		
+		
 		availableDecisions.add(cheapest);
 		
 		
 		//We keep going until the next decision would get us a win.
 		//Then after we deal with that next winning decision.
 		while (!IsWin(cheapest)) {
-			TryTakeDecision(cheapest);
+			//Do the decision taking
+			TakeDecision(cheapest, boardSize);
 			
+			//Add the new decision possibilities to the availablelist
+			availableDecisions.addAll(cheapest.children);
+			
+			//Setup for the next decisions
 			cheapest = availableDecisions.first();
+			if (cheapest.cost > GIVEUP_COST) { 
+				System.out.println("Could not find a solution, gave up");
+				return null;
+			}
 			availableDecisions.remove(cheapest);
 		}
 		
-		return null;	//No path found, shouldn't really happen.
+		return cheapest;
+		
 	}
 	
 	//Returns true if at the node, A and B are at the same place
@@ -64,13 +157,50 @@ public class Main {
 	}
 	
 	/** Executes the decision at next
-	 * Fills out next's possible decisions & costs, then returns
-	 *  the valid ones in the set.
+	 * Fills out next's possible decisions & costs
+	 * Check the new possibilities by just accessing next's
+	 * children
 	 * @param next The node that should be taken.
-	 * @return The set of new nodes that can be taken as a result of this decision.
+	 * 
 	 */
-	private Set<Node> TryTakeDecision(Node next)
-	
+	private void TakeDecision(Node next, int boardSize) {
+		//Move left
+		if (next.x > 1) {
+			//Note: the Node(node) constructor already has copied
+			// most of next's data, including setting next as parent.
+			Node westNode 	= new Node(next);
+			westNode.cost 	= next.cost + 4;
+			westNode.x 		= next.x  - 1;
+			westNode.ty 	= next.ty - 1;
+			next.children	.add(westNode);
+		}
+		//Move right
+		if (next.x < boardSize) {
+			Node eastNode	= new Node(next);
+			eastNode.cost 	= next.cost + 5;
+			eastNode.x		= next.x  + 1;
+			eastNode.ty		= next.ty + 1;
+			next.children	.add(eastNode);
+		}
+		//Move up
+		if (next.y > 1) {
+			Node northNode	= new Node(next);
+			northNode.cost	= next.cost + 3;
+			northNode.y		= next.y  - 1;
+			northNode.tx	= next.tx + 1;
+			next.children	.add(northNode);
+		}
+		if (next.y < boardSize) {
+			Node southNode	= new Node(next);
+			southNode.cost	= next.cost + 6;
+			southNode.y 	= next.y  + 1;
+			southNode.tx	= next.tx - 1;
+			next.children	.add(southNode);
+		}
+		
+		
+	}
+		
 	
 	
 	
--- CS460A1_UniformCostSearch/src/ogrady/fintan/cs460a1/Node.java
+++ CS460A1_UniformCostSearch/src/ogrady/fintan/cs460a1/Node.java
@@ -1,12 +1,14 @@
 package ogrady.fintan.cs460a1;
 
+import java.util.ArrayList;
+
 /** A node represents a possible decision in the tree.
  * As these are discovered, they are added to the total tree with the 
  * total (uniform) cost to reach them included.
  * 
  * At every node, A can decide to move in one of the 4 cardinal directions,
- *  so we can just hardcode them, and just do a little sanity checking to
- *  not go through walls. An excessively large cost, perhaps.
+ *  unless the node is on an edge. We will add these possibilities to the
+ *  list of them within the loop.
  *  
  *  Note that multiple nodes can occur for a single (x,y) position. If A
  *   moves away and then returns to this same position, or if a different
@@ -15,7 +17,7 @@ package ogrady.fintan.cs460a1;
  *  
  * @author Remulasce
  */
-public class Node {
+public class Node {	
 	//Where this node is on the board
 	public int x, y;
 	
@@ -34,8 +36,21 @@ public class Node {
 	//The next possible nodes.
 	//These will be filled with non-null Nodes once they
 	// are actually pursued.
-	public Node N;
-	public Node S;
-	public Node E;
-	public Node W;
+	public ArrayList<Node> children = new ArrayList<Node>(4);
+	
+	
+	/**Most nodes are closely related to their parents, so a copy of the parent
+	 * will be pretty close to what a new child node needs
+	 * This will automatically set the parent.
+	 * */
+	public Node() {}
+	public Node(Node parent) {
+		this.x = parent.x;
+		this.y = parent.y;
+		this.tx = parent.tx;
+		this.ty = parent.ty;
+		this.cost = parent.cost;
+		this.parent = parent;
+	}
+	
 }
